fab1acf91aa522c7b93901c56094697dc2a0e9d2
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-02 03:44:17 -0700

    fix bug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,12 @@
 import sys
 if __name__ == "__main__":
     size = len(sys.argv)
-    if (size != 2):
+    if (size == 1):
         print("{} arguments.".format(size - 1))
+    elif (size == 2):
+        print("{} argument:".format(size - 1))
     else:
-        print("{} argument.".format(size - 1))
+        print("{} arguments:".format(size - 1))
 
     for i in range(1, size):
         print("{}: {}".format(i, sys.argv[i]))
