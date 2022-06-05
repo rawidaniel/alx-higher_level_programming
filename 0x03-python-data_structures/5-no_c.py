cb11d7e044866589a3743cabaa8c9c361b54b6fb
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-05 05:25:28 -0700

    fix output error

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_str = my_string[:]
     length = len(my_string)
     i = 0
     if my_string is not None:
         while i < length:
-            if new_str[i] == "c" or new_str[i] == "C":
-                new_str = new_str[0:i] + new_str[i + 1:]
+            if my_string[i] == "c" or my_string[i] == "C":
+                my_string = my_string[0:i] + my_string[i + 1:]
                 length -= 1
             i += 1
-        return new_str
+        return my_string
