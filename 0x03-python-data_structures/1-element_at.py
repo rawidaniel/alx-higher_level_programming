df0f97aa3f360d28c360efebab4acbc3d90dc805
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-05 01:39:31 -0700

    fix output error

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     i = 0
-    if idx < 0 or idx > len(my_list):
+    if idx < 0:
+        return None
+    if idx > len(my_list):
         return None
     while i != idx:
         i += 1
