61bdf2fc383ce93532a61574dc61e30d837f03be
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-05 05:00:22 -0700

    fix bug

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 def no_c(my_string):
     new_str = my_string[:]
+    length = len(my_string)
+    i = 0
     if my_string is not None:
-        for i in range(0, len(new_str)):
+        while i < length:
             if new_str[i] == "c" or new_str[i] == "C":
                 new_str = new_str[0:i] + new_str[i + 1:]
-                return new_str
+                length -= 1
+            else:
+                pass
+            i += 1
+        return new_str
