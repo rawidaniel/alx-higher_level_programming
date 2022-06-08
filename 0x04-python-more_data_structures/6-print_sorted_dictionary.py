506328d53b5c2ca8323e178c7df81f83d43014d7
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-08 05:25:38 -0700

    fix output error

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    if a_dictionary is not None:
-        for k, v in sorted(a_dictionary.items()):
-            print(f"{k} : {v}")
-    return None
+    for k, v in sorted(a_dictionary.items()):
+        print(f"{k} : {v}")
