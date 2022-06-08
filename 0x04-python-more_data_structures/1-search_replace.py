1c2ab0454d1486fc2d4d1f914264a35e75ca5bd2
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-08 03:36:05 -0700

    fix output error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is not None:
-        new_list = my_list[:]
-        new_list[search - 1] = replace
-        return new_list
-    return None
+    new_list = my_list[:]
+    new_list[search - 1] = replace
+    return new_list
