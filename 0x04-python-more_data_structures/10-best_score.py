54c531992968e58dbeed294fef599e8692ebbe46
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-08 06:37:45 -0700

    fix output error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        biggest = 0
-        big_key = ""
-        for k, v in a_dictionary.items():
-            if v > biggest:
-                biggest = v
-                big_key = k
-        return big_key
-    return None
+    if a_dictionary:
+        return(max(a_dictionary, key=a_dictionary.get))
