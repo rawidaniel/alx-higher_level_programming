d96f870cdfd487a41d108620223346e812f0bfb2
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-20 06:59:51 -0700

    fix output error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,13 +3,8 @@ import sys
 
 
 def safe_function(fct, *args):
-    new_list = []
-    for arg in args:
-        new_list.append(arg)
     try:
-        return fct(new_list[0], new_list[1])
-    except ZeroDivisionError as e:
-        print(f"Exception: {e}", file=sys.stderr)
-    except IndexError as e:
-        print(f"Exception: {e}", file=sys.stderr)
-    return None
+        return fct(*args)
+    except (IndexError, ValueError, ZeroDivisionError, TypeError) as e:
+        sys.stderr.write(f"Exception: {e}\n".format(e))
+        return None
