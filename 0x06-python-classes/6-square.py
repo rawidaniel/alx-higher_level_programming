2879f97c8dec7aa4668820c90dc7b9f1c94d1fc3
Rawi Daniel <rawidaniel96@gmail.com>
2022-06-21 09:36:47 -0700

    fix bug

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,54 +1,94 @@
 #!/usr/bin/python3
-"Define a class Square"""
+"""
+Module 6-square
+Defines class Square with private size and position; and public area
+Can access and update size and position
+Can print to stdout the square using #'s
+"""
 
 
 class Square:
-    """Initialize a data,check the type of the size and
-        raise TypeError if it is not and check the value of
-        the size to be greater or equals to zero or raise
-        ValueError if it is not. check the type of tuple and
-        elements of the tuple either it is integer or grater than zero
-        and raise TypeError if it is not.
     """
+    class Square definition
+    Args:
+        size (int): size of a side in square
+    Functions:
+        __init__(self, size, position)
+        size(self)
+        size(self, value)
+        position(self)
+        position(self, value)
+        area(self)
+        my_print(self)
+    """
+
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
-    """Retrieve and update data"""
+        """
+        Initializes square
+        Attributes:
+            size (int): defaults to 0 if none; don't use __size to call setter
+            position (int): tuple of two positive integers
+        """
+        self.size = size
+        self.position = position
+
     @property
     def size(self):
+        """"
+        Getter
+        Return: size
+        """
         return self.__size
-    """Retrieve and update data"""
-    @property
-    def position(self):
-        return self.__position
-    """Set the property's value"""
+
     @size.setter
     def size(self, value):
-
-        if not isinstance(value, int):
+        """
+        Setter
+        Args:
+            value: sets size to value if int and >= 0
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    """Set the property's value"""
-    @size.setter
-    def size(self, value):
-        if len(value) != 2 or value[0] < 0 or value[1] < 0 or \
-           type(value) is not tuple or type(value[0]) is not int or \
-           type(value[1]) is not int:
+
+    @property
+    def position(self):
+        """"
+        Getter
+        Return: position
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """
+        Setter
+        Args:
+            value: sets position to tuple if value is tuple of 2 positive ints
+        """
+        if type(value) is not tuple or len(value) != 2 or \
+           type(value[0]) is not int or type(value[1]) is not int or \
+           value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-    """Calculate the area of the square."""
+
     def area(self):
-        return (self.size) ** 2
-    """prints in stdout the square with the character #"""
+        """
+        Calculates area of square
+        Returns:
+            area
+        """
+        return (self.__size)**2
+
     def my_print(self):
         if self.__size == 0:
-            print()
+            print("")
         else:
             print("\n" * self.__position[1], end="")
-            print("\n".join(["#" * self.__size if self.__position[1] > 0 else
-                             " " * self.__position[0] + "#" * self.__size
+            print("\n".join([" " * self.__position[0] +
+                             "#" * self.__size
                              for rows in range(self.__size)]))
