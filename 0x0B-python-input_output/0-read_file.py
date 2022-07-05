c7a2ce2abb95cedb830c6d4708a7fd3c8ade5c06
Rawi Daniel <rawidaniel96@gmail.com>
2022-07-05 07:18:03 -0700

    fix output error

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
 Module 0-read_file
-Contain method that read a text file and print to stdout
+Contain function that read a text file and print to stdout
 """
 
 
 def read_file(filename=""):
     """read from tetx file and print to stdout"""
-    with open(filename, 'r') as file:
+    with open(filename, mode='r', encoding="utf-8") as file:
         print(file.read().strip('\n'))
