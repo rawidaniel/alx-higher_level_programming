c969dab551a4f050f4d3546168c4beffd40c5078
Rawi Daniel <rawidaniel96@gmail.com>
2022-08-11 07:18:46 -0700

    fix output error

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,7 @@
 -- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
 -- should display score and the number of records for this score with the label number
 
-SELECT  DISTINCT score, count(score) number
+SELECT score, count(score) number
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
