dab2c6a44be86ac9aaa083f38686e8d1b919f399
Rawi Daniel <rawidaniel96@gmail.com>
2022-08-11 03:26:30 -0700

    create table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- creates a table called first_table in the current database
+-- description of first_table id INT and name VARCHAR(256)
+CREATE table IF
+NOT EXISTS first_table(
+	id INT,
+	name VARCHAR(256)
+);
