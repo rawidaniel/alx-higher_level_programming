a823648cfa9da60f248536e70c23df4041935abf
Rawi Daniel <rawidaniel96@gmail.com>
2022-08-11 07:12:05 -0700

    lists the number of records with the same score

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- lists the number of records with the same score in the table second_table of the database hbtn_0c_0
+-- should display score and the number of records for this score with the label number
+
+SELECT  DISTINCT score, count(score) number
+FROM second_table
+GROUP BY score;
