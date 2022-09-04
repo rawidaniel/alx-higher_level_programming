985945383e135e33dc9b48a319ac2754b9c970b3
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-04 08:00:06 -0700

    add table with constraints

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,10 @@
+-- creates the table force_name on your MySQL server
+-- table force_name has attribute id (INT0 and name VARCHAR(256)
+-- which can't be null
+-- The database name will be passed as an argument of the mysql command
+-- If the table force_name already exists, your script should not fail
+CREATE TABLE IF
+NOT EXISTS force_name(
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
