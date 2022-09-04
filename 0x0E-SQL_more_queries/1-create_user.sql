713d9ef93d8189e488a9832790319457ecf5b40d
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-04 07:36:44 -0700

    fix typo error

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -5,4 +5,4 @@
 CREATE USER IF
 NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILAGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
