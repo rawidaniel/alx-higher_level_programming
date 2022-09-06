5378da9d1d0b4419f9eb01edf42a240474bad665
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-06 07:01:10 -0700

    export a named function

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,16 @@ const val = 0;
 if (!args[2] || parseInt(args[2]) === 1) {
   console.log(val);
 } else {
-  const newArray = args.sort();
-  newArray.pop();
-  console.log(newArray.pop());
+  let first_max = 0;
+  let second_max = 0;
+  for (const i of args) {
+    if (i > first_max) {
+      first_max = i;
+    }
+  }
+  for (const i of args) {
+    if (i > second_max && i !== first_max) {
+      second_max = i;
+    }
+  }
 }
