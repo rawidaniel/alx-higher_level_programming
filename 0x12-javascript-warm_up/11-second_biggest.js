5ff5a5ab4d0c71cb0251001edf973b3caf076001
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-06 23:54:53 -0700

    fix output error

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 const val = 0;
-if (!args[2] || parseInt(args[2]) === 1) {
+if (!args[2] || args.length === 3) {
   console.log(val);
 } else {
-  let first_max = 0;
-  let second_max = 0;
-  for (const i of args) {
-    if (i > first_max) {
-      first_max = i;
-    }
-  }
-  for (const i of args) {
-    if (i > second_max && i !== first_max) {
-      second_max = i;
-    }
-  }
+  args.splice(0, 2);
+  args.sort();
+  console.log(args[args.length - 2]);
 }
