f9a5d43b42b6ac64b8b0b3943f2f66cec91fe40f
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-07 00:12:00 -0700

    fix minor bug

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,13 @@
 #!/usr/bin/node
-const args = process.argv;
-const val = 0;
-if (!args[2] || args.length === 3) {
-  console.log(val);
-} else {
-  args.splice(0, 2);
-  args.sort();
-  console.log(args[args.length - 2]);
+const args = process.argv.slice(2);
+
+function secondBiggestInteger (array) {
+  if (array.length < 2) {
+    return 0;
+  } else {
+    array.sort((x, y) => x - y);
+    array.pop();
+    return array.pop();
+  }
 }
+console.log(secondBiggestInteger(args));
