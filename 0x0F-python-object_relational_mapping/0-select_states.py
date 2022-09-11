6fd0261edc3bff0b91941e8edf74f40a7b0f3552
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-10 23:27:09 -0700

    fix bug

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,16 @@
 Module 0-select_states
 lists all states from the database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
 
 if __name__ == "__main__":
+    
+    import MySQLdb
+    import sys
+
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+                         passwd=sys.argv[2], db=sys.argv[3])
     myCursor = db.cursor()
-    myCursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    myCursor.execute("SELECT * FROM states ORDER BY states.id ASC;")
     rows = myCursor.fetchall()
     for row in rows:
         print(row)
