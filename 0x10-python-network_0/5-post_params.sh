84b660ad9ca3436393576d95660c27bba3de3d2b
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-22 22:27:08 +0300

    fix bug

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #takes in a URL, sends a POST request and displays the body of the response
-curl -s "$1" -X POST -d  "gmail.com&subject=I will always be here for PLD"
+curl -s "$1" -X POST -d  "email=test@gmail.com&subject=I will always be here for PLD"
+
