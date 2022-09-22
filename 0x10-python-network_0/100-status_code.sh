5848767554d75a672ba7a1dc2810560174100903
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-22 22:52:05 +0300

    fix bug

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a URL and displays only the status code of the response
-curl -sI "$1" | grep "HTTP/" | cut -f2 -d " "
+curl -o /dev/null -s -w "%{http_code}\n" "$1"
