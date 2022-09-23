5389b5ce1a1fcc679a779bbc4b0c5c2b917904ea
Rawi Daniel <rawidaniel96@gmail.com>
2022-09-23 23:09:03 +0300

    fix output error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,12 +3,12 @@ Module 0-hbtn_status
 fetches https://alx-intranet.hbtn.io/status
 """
 import urllib.request
-
-with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as req:
-    respo = req.read()
-    type_of_class = type(respo)
-    data = respo.decode('UTF-8')
-    print("Body response:")
-    print(f"\t- type: {type_of_class}")
-    print(f"\t- content: {respo}")
-    print(f"\t- utf8 content: {data}")
+if __name__ == "__main__":
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as req:
+        respo = req.read()
+        type_of_class = type(respo)
+        data = respo.decode('UTF-8')
+        print("Body response:")
+        print(f"\t- type: {type_of_class}")
+        print(f"\t- content: {respo}")
+        print(f"\t- utf8 content: {data}")
