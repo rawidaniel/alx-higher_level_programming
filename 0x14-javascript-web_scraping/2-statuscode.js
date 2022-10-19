1e941cea9b957cfc58d4caaed60a3619b6fd8fb8
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-19 10:22:05 +0300

    fix minor bug

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 
-request(process.argv[2], function (err, response, body) {
+request(process.argv[2], function (err, response) {
   if (err) {
     console.log(err);
   } else {
