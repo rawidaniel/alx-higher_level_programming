ed7fc47ddf69a548967959d1cf0cd39c5846aafb
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-19 22:31:02 +0300

    fix minor error

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
 const request = require('request');
-let url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+const id = process.argv[2];
+const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 request(url, function (error, response, body) {
   if (error) {
     console.error(error);
