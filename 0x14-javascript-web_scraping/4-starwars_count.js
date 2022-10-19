7c6890bc75f66131ff4b33bfae3aa8257eb4611f
Rawi Daniel <rawidaniel96@gmail.com>
2022-10-20 01:31:03 +0300

    reterive number of character

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    let count = 0;
+    films.forEach((element) => {
+      element.characters.forEach((ele) => {
+        if (ele.includes('18')) {
+          count++;
+        }
+      });
+    });
+    console.log(count);
+  }
+});
